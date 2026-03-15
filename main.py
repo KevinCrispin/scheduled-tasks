1eb9fecf682111e969d728aeb3012e314c3f2720
KevinCrispin <kevinextraemail442@gmail.com>
2026-03-15 15:27:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,42 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
+import requests
 import os
+from twilio.rest import Client
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+sid = os.environ.get("sid")
+auth = os.environ.get("auth")
+fn = "whatsapp:+916360530768"
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+client = Client(sid,auth)
+ 
+endpoint = "https://api.openweathermap.org/data/2.5/forecast"
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+api_params = {
+    "lat":46.947975,
+    "lon": 7.447447,
+    "cnt" : 4,
+    "appid": "360c4f5b8e9c2bc2c41e5496ac12106a"
+}
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+response = requests.get(endpoint,api_params)
+output = response.json()
+print(output)
+
+gonna_Rain = False
+for time in output["list"]:
+    if int(time["weather"][0]["id"])>700:
+        print(time["dt_txt"])
+
+for i in range(0,20):
+    if(not gonna_Rain):
+        client.messages.create(
+                from_= "whatsapp:+14155238886",
+                to= fn,
+                body="It is not going to rain"
+                )
+    else:
+        client.messages.create(
+                from_= "whatsapp:+14155238886",
+                to= fn,
+                body="It is going to rain, carry an umbrella"
+                )
+    
